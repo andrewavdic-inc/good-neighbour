84c420c9907c26299f65adb1c2dcc81b2e897f53
Andrew <andrew.avdic@gmail.com>
2026-04-17 22:58:25 -0400

    Deleted line 11

--- good-neighbour-portal/src/App.jsx
+++ good-neighbour-portal/src/App.jsx
@@ -8,7 +8,6 @@ import { getFirestore, collection, onSnapshot, doc, setDoc, updateDoc, deleteDoc
 
 // --- FIREBASE INITIALIZATION ---
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
